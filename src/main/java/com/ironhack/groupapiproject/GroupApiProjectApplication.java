3da94ecfce72f7515e39b5411364f815a1c62ccb
Elena Piaggio <elenapiaggio@gmail.com>
2022-08-27 17:13:19 +0200

    first commit

---
+++ src/main/java/com/ironhack/groupapiproject/GroupApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.groupapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupApiProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GroupApiProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ironhack/groupapiproject/GroupApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.groupapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupApiProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
