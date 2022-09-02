916fbe59b5f516227c5ce1dad0564e99d23c8f7a
EidenAndreu <eidenreinaldo@gmail.com>
2022-09-02 17:48:09 +0200

    actualizado

--- src/main/java/com/ironhack/groupapiproject/model/SakuraCard.java
+++ src/main/java/com/ironhack/groupapiproject/model/SakuraCard.java
@@ -15,9 +15,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class SakuraCard {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
-    private long id;
+    private String _id;
 
     private int cardNumber;
     private String spanishName;
@@ -25,4 +23,11 @@ public class SakuraCard {
     private String clowCard;
     private String sakuraCard;
 
+    public SakuraCard(int cardNumber, String spanishName, String englishName, String clowCard, String sakuraCard) {
+        this.cardNumber = cardNumber;
+        this.spanishName = spanishName;
+        this.englishName = englishName;
+        this.clowCard = clowCard;
+        this.sakuraCard = sakuraCard;
+    }
 }
