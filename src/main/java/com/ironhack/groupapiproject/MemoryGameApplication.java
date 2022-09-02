0576af3d7cdab92dc8dfa1b35a795698adddc015
EidenAndreu <eidenreinaldo@gmail.com>
2022-09-02 16:43:41 +0200

    Menu phrases

--- src/main/java/com/ironhack/groupapiproject/MemoryGameApplication.java
+++ src/main/java/com/ironhack/groupapiproject/MemoryGameApplication.java
@@ -1,7 +1,6 @@
 package com.ironhack.groupapiproject;
 
 import com.ironhack.groupapiproject.menu.SakuraMenu;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
--- src/main/java/com/ironhack/groupapiproject/menu/SakuraMenu.java
+++ src/main/java/com/ironhack/groupapiproject/menu/SakuraMenu.java
@@ -35,36 +35,36 @@ public final class SakuraMenu{
                 case CASE_2 -> System.out.println("Get a Sakura card by id");
                 case CASE_3 -> System.out.println("Get a Sakura card by name in english");
                 case CASE_4 -> System.out.println("Get a Sakura card by name in spanish");
-                case CASE_5 -> System.out.println("Other options");
+                case CASE_5 -> System.out.println("Get a random Sakura card");
                 case EXIT -> exit = true;
-                default -> System.out.println("Command not recognized. Please try again.");
+                default -> System.out.println("Kero can not understand this command :( " +
+                        "Please try again.");
             }
         } while (!exit);
 
-        System.out.println("Thank you for using the application.");
+        System.out.println("Thanks for visiting me :)");
+        System.out.println("See you next time, Cards Collector!");
     }
 
     private void printWelcome() {
-        System.out.println(" ");
         System.out.println("======================================================================");
         System.out.println(" ");
         System.out.println("  Welcome to the Sakura Cards Collector!");
         System.out.println(" ");
         System.out.println("======================================================================");
         System.out.println(" ");
+        System.out.println(" My name is Kerberos, but you can call me Kero. How can I help you?");
+
     }
 
     private void printCommandRequest() {
-        System.out.println(" ");
-        System.out.println("======================================================================");
-        System.out.println(" ");
-        System.out.println("  Please select a command from the following options:");
+        System.out.println(" Please, select a command from the following options:");
         System.out.println(" ");
         System.out.println("    1. Get all Sakura cards");
         System.out.println("    2. Get a Sakura card by id");
         System.out.println("    3. Get a Sakura card by name in english");
         System.out.println("    4. Get a Sakura card by name in spanish");
-        System.out.println("    5. Other options ");
+        System.out.println("    5. Get a random Sakura card ");
         System.out.println("    0. Exit the application");
         System.out.println(" ");
         System.out.print("  Enter your command [0-5]: ");
